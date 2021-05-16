07adbef92a13c0ffe27f27ca0c4404b1d314084a
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-05-16 13:49:28 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable         
 
-  validates :nickname, presence: true
-  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates_format_of :password, :password_confirmation, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+         
+  validates :nickname, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {'やマ田'}
+    first_name            {'たロ宇'}
+    kana_last_name        {'ヤマダ'}
+    kana_first_name       {'タロウ'}
+    birthday              {'2000-01-01'}
+  end
+end
