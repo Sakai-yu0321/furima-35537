8b5b1391ad2da129d9f66d6858c28a11f547eaf0
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-05-17 11:52:37 +0900

    画像ファイルの保存を許可

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product_name,
+                                 :product_description,
+                                 :category_id,
+                                 :product_status_id,
+                                 :shipping_charge_id,
+                                 :prefecture_id,
+                                 :day_to_ship_id,
+                                 :price,
+                                 :image).merge(user_id: current_user.id)
+  end
 end
