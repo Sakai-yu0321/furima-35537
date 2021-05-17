c1328dcaa10999e237e3c64dcca7e17b5b620d92
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-05-17 17:43:04 +0900

    ActiveHashモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,18 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :product_status, :shipping_charge, :prefecture, :day_to_ship
+  belongs_to :category, :product_status, :shipping_charge, :prefecture, :day_to_ship
 
   has_one_attached :image
-  validates :product_name, presence: true
+  validates :image,               presence: true
+  validates :product_name,        presence: true
   validates :product_description, presence: true
-  validates :category_id, presence: true
-  validates :product_status_id, presence: true
-  validates :shipping_charge_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :day_to_ship_id, presence: true
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :category_id,         presence: true, numericality: { other_than: 1 }
+  validates :product_status_id,   presence: true, numericality: { other_than: 1 }
+  validates :shipping_charge_id,  presence: true, numericality: { other_than: 1 }
+  validates :prefecture_id,       presence: true
+  validates :day_to_ship_id,      presence: true, numericality: { other_than: 1 }
+  validates :price,               presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
