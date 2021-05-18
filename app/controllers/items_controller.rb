53c903ae050f6832f9e3f9af7b17760e9ebc404d
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-05-18 17:16:36 +0900

    show.html.erbの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,10 @@ class ItemsController < ApplicationController
     @items = Item.all.order(created_at: 'DESC')
   end
 
+  def show
+    @item = Item.find_by(user_id: params[:id])
+  end
+
   def new
     @item = Item.new
   end
