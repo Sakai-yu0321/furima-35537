d617dac949fb9c48f3e980df9e896c189fea4a08
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-05-18 12:21:09 +0900

    rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ApplicationRecord
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :product_status, :shipping_charge, :prefecture, :day_to_ship
 
@@ -12,7 +11,8 @@ class Item < ApplicationRecord
   validates :shipping_charge_id,  presence: true, numericality: { other_than: 1 }
   validates :prefecture_id,       presence: true
   validates :day_to_ship_id,      presence: true, numericality: { other_than: 1 }
-  validates :price,               presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :price,               presence: true,
+                                  numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     shipping_charge_id  { '2' }
     prefecture_id       { '2' }
     day_to_ship_id      { '2' }
-    price               { Faker::Number.between(from: 300, to: 9999999) }
+    price               { Faker::Number.between(from: 300, to: 9_999_999) }
     association :user
 
     after(:build) do |item|
