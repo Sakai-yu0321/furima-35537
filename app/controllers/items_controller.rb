b581e7398f85b29b3b26955e57e2e5ff51707053
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-05-18 21:46:51 +0900

    エラーメッセージの表示とアクセス制限の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: 'DESC')
@@ -27,9 +28,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.save
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
       redirect_to(item_path)
     else
       render(:edit)
@@ -48,5 +49,12 @@ class ItemsController < ApplicationController
                                  :day_to_ship_id,
                                  :price,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 end
